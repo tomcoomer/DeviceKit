b04ffa68591714ebbc6eeea64b94403a15e3eb1c
Tom Coomer <tom@Toms-Mac-Pro.home>
2015-08-25 17:56:42 +0100

    Initial Commit

---
+++ DeviceKit/DeviceKit.h
@@ -0,0 +1,19 @@
+//
+//  DeviceKit.h
+//  DeviceKit
+//
+//  Created by Tom Coomer on 25/08/2015.
+//  Copyright (c) 2015 Tom Coomer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceKit.
+FOUNDATION_EXPORT double DeviceKitVersionNumber;
+
+//! Project version string for DeviceKit.
+FOUNDATION_EXPORT const unsigned char DeviceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceKit/PublicHeader.h>
+
+

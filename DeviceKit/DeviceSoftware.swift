63e0b491a57be2bdb2d5e41a60eb2c0aa55dc70c
Tom Coomer <tomcoomer@me.com>
2017-10-11 22:36:13 +0100

    • Updated to Swift 4

--- DeviceKit/DeviceSoftware.swift
+++ DeviceKit/DeviceSoftware.swift
@@ -11,14 +11,16 @@ import Foundation
 public class DeviceSoftware {
     
     public init (){
-        println("Hardware Class has been initialised")
+        print("Software Class has been initialised")
     }
     
     struct Version{
-        static let SYS_VERSION_FLOAT = (UIDevice.currentDevice().systemVersion as NSString).floatValue
+        static let SYS_VERSION_FLOAT = (UIDevice.current.systemVersion as NSString).floatValue
         static let iOS7 = (Version.SYS_VERSION_FLOAT < 8.0 && Version.SYS_VERSION_FLOAT >= 7.0)
         static let iOS8 = (Version.SYS_VERSION_FLOAT >= 8.0 && Version.SYS_VERSION_FLOAT < 9.0)
         static let iOS9 = (Version.SYS_VERSION_FLOAT >= 9.0 && Version.SYS_VERSION_FLOAT < 10.0)
+        static let iOS10 = (Version.SYS_VERSION_FLOAT >= 10.0 && Version.SYS_VERSION_FLOAT < 11.0)
+        static let iOS11 = (Version.SYS_VERSION_FLOAT >= 11.0)
     }
     
     public func IS_IOS_7() -> Bool {
@@ -31,6 +33,14 @@ public class DeviceSoftware {
     
     public func IS_IOS_9() -> Bool {
         return Version.iOS9
+    }
+    
+    public func IS_IOS_10() -> Bool {
+        return Version.iOS10
+    }
+    
+    public func IS_IOS_11() -> Bool {
+        return Version.iOS11
     }
     
     public func softwareVerstion() -> Float {
--- DeviceKit/HardwareType.swift
+++ DeviceKit/HardwareType.swift
@@ -12,7 +12,7 @@ import Foundation
 public class HardwareType {
     
     public init (){
-        println("Hardware Class has been initialised")
+        print("Hardware Class has been initialised")
     }
     
     enum UIUserInterfaceIdiom : Int
@@ -24,19 +24,19 @@ public class HardwareType {
     
     struct ScreenSize
     {
-        static let SCREEN_WIDTH         = UIScreen.mainScreen().bounds.size.width
-        static let SCREEN_HEIGHT        = UIScreen.mainScreen().bounds.size.height
+        static let SCREEN_WIDTH         = UIScreen.main.bounds.size.width
+        static let SCREEN_HEIGHT        = UIScreen.main.bounds.size.height
         static let SCREEN_MAX_LENGTH    = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
         static let SCREEN_MIN_LENGTH    = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
     }
     
    public struct DeviceType
     {
-        static let IS_IPHONE_4_OR_LESS  = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
-        static let IS_IPHONE_5          = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
-        static let IS_IPHONE_6          = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
-        static let IS_IPHONE_6P         = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
-        static let IS_IPAD              = UIDevice.currentDevice().userInterfaceIdiom == .Pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
+        static let IS_IPHONE_4_OR_LESS  = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
+        static let IS_IPHONE_5          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
+        static let IS_IPHONE_6          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
+        static let IS_IPHONE_6P         = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
+        static let IS_IPAD              = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
     }
     
 //    private var _device: String?
@@ -72,7 +72,7 @@ public class HardwareType {
     
     //Device Type
     public func IS_IPHONE() -> Bool {
-        return UIDevice.currentDevice().userInterfaceIdiom == .Phone
+        return UIDevice.current.userInterfaceIdiom == .phone
     }
     
     
